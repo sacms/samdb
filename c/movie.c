24f3b675f9052c5da2b52559bd213a53af75a99d
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2023-07-15 18:20:32 +0600

    Create movie.c

---
+++ c/movie.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+struct string {
+    char *ptr;
+    size_t len;
+};
+
+void init_string(struct string *s) {
+    s->len = 0;
+    s->ptr = malloc(s->len + 1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    s->ptr[0] = '\0';
+}
+
+size_t write_callback(void *ptr, size_t size, size_t nmemb, struct string *s) {
+    size_t new_len = s->len + size * nmemb;
+    s->ptr = realloc(s->ptr, new_len + 1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "Memory reallocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(s->ptr + s->len, ptr, size * nmemb);
+    s->ptr[new_len] = '\0';
+    s->len = new_len;
+    return size * nmemb;
+}
+
+char *sacms(const char *id, const char *accessToken) {
+    CURL *curl;
+    CURLcode res;
+    struct string response;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        char url[256];
+        snprintf(url, sizeof(url), "https://samdb.eu.org/api/imdb?id=%s", id);
+
+        init_string(&response);
+        
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        struct curl_slist *headers = NULL;
+        char auth_header[128];
+        snprintf(auth_header, sizeof(auth_header), "Authorization: Bearer %s", accessToken);
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        headers = curl_slist_append(headers, auth_header);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(headers);
+        
+        return response.ptr;
+    }
+
+    curl_global_cleanup();
+    return NULL;
+}
+
+int main(void) {
+    const char *accessToken = ""; // Enter your Bearer Token
+    char *response = sacms("tt1630029", accessToken);
+    if (response) {
+        printf("%s\n", response);
+        free(response);
+    }
+
+    return 0;
+}
