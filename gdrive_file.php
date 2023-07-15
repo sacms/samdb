24de289c9e7f26f77ae5d40f059c103bb9f6f978
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2023-07-15 17:50:10 +0600

    Update gdrive_file.php

--- gdrive_file.php
+++ gdrive_file.php
@@ -1,9 +1,12 @@
 <?php 
 function sacms($id, $accessToken) {
     $url = 'https://samdb.eu.org/api/GDfile?id='.$id;
+    $data = array(
+    );
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
     $authorization = 'Authorization: Bearer '.$accessToken;
     $headers = array(
         'Content-Type: application/json',
@@ -12,6 +15,7 @@ function sacms($id, $accessToken) {
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     $response = curl_exec($ch);
     curl_close($ch);
+    return $response;
 }
 
 $accessToken = ''; // Enter your Bearer Token
