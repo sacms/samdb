d6621a1775a27e8401f13c636c0e0bbae605d717
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2023-08-14 14:18:05 +0600

    Create GDfileController.php

---
+++ laravel/app/Http/Controllers/GDfileController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MovieController extends Controller
+{
+    public function getSacmsData($id, $accessToken)
+    {
+        $url = 'https://samdb.eu.org/api/GDfile?id='.$id;
+        $data = array();
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        $authorization = 'Authorization: Bearer '.$accessToken;
+        $headers = array(
+            'Content-Type: application/json',
+            $authorization
+        );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+}
