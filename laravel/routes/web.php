eb957ef7d65c55fb599afc5143e27f251bc1d9c4
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2023-08-14 14:21:50 +0600

    Update web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,8 @@
 
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\IpController;
+use App\Http\Controllers\GDfileController;
 
 Route::get('/fetch-movie', function () {
     return view('fetch-movie');
@@ -15,4 +17,33 @@ Route::post('/fetch-movie', function () {
     $response = $movieController->getSacmsData($movieId, $accessToken);
 
     return view('fetch-movie', ['response' => $response]);
+});
+
+
+Route::get('/fetch-ip', function () {
+    return view('fetch-ip');
+})->name('fetch-ip');
+
+Route::post('/fetch-ip', function () {
+    $movieId = request()->input('ip');
+    $accessToken = request()->input('accessToken');
+
+    $movieController = new IpController();
+    $response = $movieController->getSacmsData($movieId, $accessToken);
+
+    return view('fetch-ip', ['response' => $response]);
+});
+
+Route::get('/fetch-gd', function () {
+    return view('fetch-gd');
+})->name('fetch-gd');
+
+Route::post('/fetch-gd', function () {
+    $movieId = request()->input('fileId');
+    $accessToken = request()->input('accessToken');
+
+    $movieController = new GDfileController();
+    $response = $movieController->getSacmsData($movieId, $accessToken);
+
+    return view('fetch-gd', ['response' => $response]);
 });
